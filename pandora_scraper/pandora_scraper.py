35dd47d8ac083ee0f50e9b16ec24e5d3faa074c3
Amber <847126496@qq.com>
2020-02-04 17:05:01 +0800

    fully functioning data capturing bot

--- pandora_scraper/pandora_scraper.py
+++ pandora_scraper/pandora_scraper.py
@@ -4,6 +4,7 @@ from selenium import webdriver
 import time, re
 from secret import username, password
 import pandas as pd
+import openpyxl
 
 #python -i pandora_scraper.py
 
@@ -12,10 +13,14 @@ class pandoraBot:
     comment_tickets = []
     inbox_tickets = []
 
+    export_colomns = ["Message_Time","Customer_Name","Enquiry","URL"]
+
+    export_path = r"C:\Users\abc\PycharmProjects\somestuff\pandora_scraper\pandora_export.xlsx"
+
     def __init__(self):
         self.driver = webdriver.Chrome(executable_path=r"C:\Users\abc\PycharmProjects\chromedriver_win32\chromedriver.exe")
 
-    def login(self):
+    def login_old(self):
         self.driver.get("https://space.sprinklr.com/new")
 
         time.sleep(7)
@@ -46,64 +51,64 @@ class pandoraBot:
         engagement_open.click()
         time.sleep(2)
 
-    def scrape_public_msgs(self):
+    def login(self):
+        self.driver.get("https://space.sprinklr.com/new")
 
-        per_ticket = self.driver.find_element_by_xpath('//article[@class = "streamEntity spr"]').text
+        time.sleep(2)
 
-        scroll_box_public = self.driver.find_element_by_xpath('//*[@id="sprEngagementWorkspace"]/div/div/div[2]/div/div[2]/div[2]/div/div/section/div')
-        scroll = self.driver.execute_script("arguments[0].scrollTo(0, arguments[0].scrollHeight)",scroll_box_public)
-
-        fb_username_ele = self.driver.find_element_by_xpath('//div[@class="msgProfileName spr-text-01 text-13 font-700 text-overflow scp"]')
-        fb_username = fb_username_ele.text
-
-
-        scroll_height = 0
-        for _ in range(4):
-            scroll_height += 260
-            self.driver.execute_script("arguments[0].scrollTo(0, {})".format(scroll_height), scroll_box_public)
-            time.sleep(0.1)
-            fb_username_ele = self.driver.find_element_by_xpath('//div[@class="msgProfileName spr-text-01 text-13 font-700 text-overflow scp"]')
-            time.sleep(0.1)
-            fb_username = fb_username_ele.text
-            print(fb_username)
-
-        scroll_box_public = self.driver.find_element_by_xpath('//*[@id="sprEngagementWorkspace"]/div/div/div[2]/div/div[2]/div[2]/div/div/section/div')
-        scroll = self.driver.execute_script("arguments[0].scrollTo(0, arguments[0].scrollHeight)",scroll_box_public)
-
-        names_ele = self.driver.find_elements_by_xpath('//div[@class="msgProfileName spr-text-01 text-13 font-700 text-overflow scp"]')
-        names = [name.text for name in names_ele if name.text != '']
-
-        #public_box = self.driver.find_element_by_xpath('//span[contains(text(),"Facebook - Public")]//*//div[@class="msgProfileName spr-text-01 text-13 font-700 text-overflow scp"]')
-        public_box = self.driver.find_element_by_xpath('//span[contains(text(),"Facebook - Public")]')
-        names_ele = public_box.find_elements_by_xpath('//div[@class="msgProfileName spr-text-01 text-13 font-700 text-overflow scp"]')
-
-
-        ##########################
-        ticket_ele = self.driver.find_elements_by_xpath('//article[@class = "streamEntity spr"]')
-        for i in ticket_ele:
-            ticket_name = i.find_element_by_xpath('.//div[@class="msgProfileName spr-text-01 text-13 font-700 text-overflow scp"]').text
-            ticket_text = i.find_element_by_xpath('.//div[@class="msgContent"]').text
+        email_input_element_not_found = True
+        while email_input_element_not_found:
             try:
-                ticket_link = i.find_element_by_xpath('.//a[@class = "msgTimeStamp msgHeaderSubText spr-text-02 txt-bd4 pull-xs-right m-l-1 msgTimeStampRenderAsLink"]').get_attribute("href")
+                email_input = self.driver.find_element_by_xpath('//*[@id="root"]/div/section[2]/section/div[1]/div/div/div/div/div/div/form/div[1]/div/input')
+                email_input_element_not_found = False
+                print("email input element found")
             except:
-                ticket_link = ""
+                print("retry finding email input element...")
+            time.sleep(0.5)
+
+        email_input.send_keys(username)
+        time.sleep(1)
+
+        next_btn = self.driver.find_element_by_xpath(
+            '//*[@id="root"]/div/section[2]/section/div[1]/div/div/div/div/div/div/form/div[1]/button')
+        time.sleep(0.5)
+        next_btn.click()
+        time.sleep(1)
+
+        pw_input_element_not_found = True
+        while pw_input_element_not_found:
             try:
-                ticket_time = i.find_element_by_xpath('.//a[contains(@aria-label,"Posted on")]').get_attribute("aria-label")
+                pw_input = self.driver.find_element_by_xpath(
+                    '//*[@id="root"]/div/section[2]/section/div[1]/div/div/div/div/div/div[2]/form/div[2]/div[1]/input')
+                print("pw input element found")
+                pw_input_element_not_found = False
             except:
-                ticket_time = ""
+                print("retry finding pw input element")
+            time.sleep(0.5)
 
+        pw_input.send_keys(password)
+        time.sleep(1)
+
+        login_btn = self.driver.find_element_by_xpath(
+            '//*[@id="root"]/div/section[2]/section/div[1]/div/div/div/div/div/div[2]/form/div[2]/button')
+        time.sleep(0.5)
+        login_btn.click()
+
+        time.sleep(2)
+
+        engagement_open_element_not_found = True
+        while engagement_open_element_not_found:
             try:
-                ticket_image = i.find_element_by_xpath('.//img[@class = "show mediaImgContent scp"]').get_attribute("src")
+                engagement_open = self.driver.find_element_by_xpath(
+                    '//*[@id="sprPage"]/div/div/div/div/div[2]/div/div/div/div/div/ul/li[1]/div[2]/div/div/div[1]/a[1]/div/div/div[1]')
+                engagement_open_element_not_found = False
+                print("engagement_open_element found")
             except:
-                ticket_image = None
+                print("retry finding engagement_open_element")
+            time.sleep(0.5)
 
-            msgtime_regex = re.compile(r'Posted on(.*)')
-            ticket_time = msgtime_regex.search(ticket_time).group()
-
-            print(ticket_name,ticket_text,ticket_link,ticket_time,"\n")
-            if ticket_image:
-                print(ticket_image,"\n")
-        ######################
+        engagement_open.click()
+        time.sleep(2)
 
     def getTicketContent(self):
 
@@ -154,48 +159,134 @@ class pandoraBot:
             # if ticket_image:
             #     print(ticket_image,"\n")
 
+    def get_dataframe(self):
 
-    def print_dataframe(self):
+        df_comment = pd.DataFrame(columns=self.export_colomns)
+        df_inbox = pd.DataFrame(columns=self.export_colomns)
 
         if len(self.comment_tickets) > 0:
-            df_comment = pd.DataFrame(self.comment_tickets,columns=["Message_Time","Customer_Name","Enquiry","URL"])
+            df_comment = pd.DataFrame(self.comment_tickets,columns=self.export_colomns)
 
             df_comment["Message_Time"] = pd.to_datetime(df_comment.Message_Time)
-            df_comment = df_comment.sort_values(by='Message_Time', ascending=False)
+            df_comment = df_comment.sort_values(by='Message_Time', ascending=True)
 
             df_comment["Message_Time"] = df_comment["Message_Time"].dt.strftime("%A, %B %d, %Y %I:%M:%S %p")
 
             print(df_comment)
             print(self.comment_tickets)
         if len(self.inbox_tickets) > 0:
-            df_inbox = pd.DataFrame(self.inbox_tickets,columns=["Message_Time","Customer_Name","Enquiry","URL"])
+            df_inbox = pd.DataFrame(self.inbox_tickets,columns=self.export_colomns)
 
             df_inbox["Message_Time"] = pd.to_datetime(df_inbox.Message_Time)
-            df_inbox = df_inbox.sort_values(by='Message_Time',ascending=False)
+            df_inbox = df_inbox.sort_values(by='Message_Time',ascending=True)
 
             df_inbox["Message_Time"] = df_inbox["Message_Time"].dt.strftime("%A, %B %d, %Y %I:%M:%S %p")
 
             print(df_inbox)
             print(self.inbox_tickets)
 
-    def test(self,scrolls = 4 ):
+        return df_comment,df_inbox
+
+
+    def test(self,scrolls = 5 ):
         scroll_box_public = self.driver.find_element_by_xpath('//*[@id="sprEngagementWorkspace"]/div/div/div[2]/div/div[2]/div[2]/div/div/section/div')
-        #scroll_box_private = a.driver.find_elements_by_xpath('//*[@id="sprEngagementWorkspace"]/div/div/div[3]/div/div[2]/div[2]/div/div/section/div')
-        scroll_height = 0
+        scroll_box_private = self.driver.find_elements_by_xpath('//*[@id="sprEngagementWorkspace"]/div/div/div[3]/div/div[2]/div[2]/div/div/section/div')
+        #scroll_box_private[0].location_once_scrolled_into_view
+        #scroll_box_public[0].location_once_scrolled_into_view
+        # random_user_private = a.driver.find_elements_by_xpath('//*[@id="sprEngagementWorkspace"]/div/div/div[3]/div/div[2]/div[2]/div/div/section/div/div/div/div/article[2]/section/section/article/div/div/section[2]/div[1]/div/div[1]/div[1]/div/div')
+        # random_user_private = a.driver.find_elements_by_xpath('//div[contains(text(),"Bleu Loi")]')
+        # random_user_private[0].location_once_scrolled_into_view
+
+        # from selenium.webdriver.common.action_chains import ActionChains
+        # ActionChains(a.driver).move_to_element(scroll_box_private).perform()
+
+        scroll_height = -260
         for _ in range(scrolls):
             scroll_height += 260
             self.driver.execute_script("arguments[0].scrollTo(0, {})".format(scroll_height), scroll_box_public)
             time.sleep(0.2)
             #a.driver.execute_script("arguments[0].scrollTo(0, {})".format(scroll_height), scroll_box_private)
-            #a.driver.execute_script("arguments[0].scrollTo(0, 260)", scroll_box_private)
+            self.driver.execute_script("arguments[0].scrollTo(0, {})".format(scroll_height),scroll_box_private[0])
             time.sleep(0.2)
             self.getTicketContent()
-        self.print_dataframe()
+        self.get_dataframe()
+
+
+    def export_to_excel(self):
+
+        export_sheet = ["Comment", "Inbox"]
+
+        # delete the previous export sheets
+        wb = openpyxl.load_workbook(self.export_path)
+        sheets = wb.sheetnames
+        for i in export_sheet:
+            if i in sheets:
+                wb.remove(wb[i])
+        wb.save(self.export_path)
+
+
+        df_comment, df_inbox = self.get_dataframe()
+
+        dfs = [df_comment,df_inbox]
+
+        for i,df in enumerate(dfs):
+            wb = openpyxl.load_workbook(self.export_path)
+            writer = pd.ExcelWriter(self.export_path, engine='openpyxl')
+            writer.book = wb
+
+            df.to_excel(writer, sheet_name=export_sheet[i], index=False)
+            writer.save()
+            writer.close()
+
+    def export_with_openpyxl(self):
+
+        #per_ticket_content = [ticket_time,ticket_name,ticket_text,ticket_link]
+
+        df_comment, df_inbox = self.get_dataframe()
+
+        if len(self.comment_tickets) > 0:
+
+            ordered_comment_tickets = []
+            for index, row in df_comment.iterrows():
+                ticket = [row[self.export_colomns[0]],row[self.export_colomns[1]],row[self.export_colomns[2]],row[self.export_colomns[3]]]
+                ordered_comment_tickets.append(ticket)
+
+            wb = openpyxl.load_workbook(self.export_path)
+            sheet_public = wb["Public"]
+
+            sheet_public.delete_rows(2, 200)
+
+            for i,each_ticket in enumerate(ordered_comment_tickets):
+                for n,each_piece_of_info in enumerate(each_ticket):
+                    sheet_public.cell(row=2+i,column=n+1).value = each_piece_of_info
+
+            wb.save(self.export_path)
+
+        if len(self.inbox_tickets) > 0:
+
+            ordered_inbox_tickets = []
+            for index, row in df_inbox.iterrows():
+                ticket = [row[self.export_colomns[0]],row[self.export_colomns[1]],row[self.export_colomns[2]],row[self.export_colomns[3]]]
+                ordered_inbox_tickets.append(ticket)
+
+            wb = openpyxl.load_workbook(self.export_path)
+            sheet_private = wb["Private"]
+
+            sheet_private.delete_rows(2,200)
+
+            for i, each_ticket in enumerate(ordered_inbox_tickets):
+                for n, each_piece_of_info in enumerate(each_ticket):
+                    sheet_private.cell(row=2+i,column=n+1).value = each_piece_of_info
+
+            wb.save(self.export_path)
+
+
 
 
 
 a = pandoraBot()
 a.login()
+
 
 
 
--- pandora_scraper/test.py
+++ pandora_scraper/test.py
@@ -2,32 +2,63 @@
 
 import re
 import pandas as pd
+import openpyxl
 
-posted = "Posted onMonday, February 03, 2020 12:07:21 AM"
+# posted = "Posted onMonday, February 03, 2020 12:07:21 AM"
+#
+# msgtime_regex = re.compile(r'Posted on(.*)')
+# ticket_time = msgtime_regex.search(posted).group(1)
+#
+# time = "Monday, February 03, 2020 12:07:32 AM"
+#
+# comment_tickets = [['Sunday, February 02, 2020 10:45:40 AM', 'Y L Chan', 'How much?', 'https://www.facebook.com/153753694819713/posts/1249108418617563/?comment_id=1250340885160983'],['Monday, February 03, 2020 11:29:26 AM', 'Chan Wing Yiu', r'https://scontent.xx.fbcdn.net/v/t1.0-9/s720x720/84003939_10157382231709440_3622552945624612864_n.jpg?_nc_cat=102&_nc_oc=AQlq6wf-34Mj-fjynxxUsqUjMdrT_ekpFIBT2Az0-3416A_X124IBKikqCPAeGvuhfY&_nc_ht=scontent.xx&oh=0fa985bb028b65bc4aaf3662ee0bddf4&oe=5EBB5493', r'https://www.facebook.com/153753694819713/posts/1249085878619817/?comment_id=1251058991755839'
+# ],['Monday, February 02, 2020 9:29:26 AM', 'Chan Wing Yiu', r'https://scontent.xx.fbcdn.net/v/t1.0-9/s720x720/84003939_10157382231709440_3622552945624612864_n.jpg?_nc_cat=102&_nc_oc=AQlq6wf-34Mj-fjynxxUsqUjMdrT_ekpFIBT2Az0-3416A_X124IBKikqCPAeGvuhfY&_nc_ht=scontent.xx&oh=0fa985bb028b65bc4aaf3662ee0bddf4&oe=5EBB5493', r'https://www.facebook.com/153753694819713/posts/1249085878619817/?comment_id=1251058991755839']]
+#
+# inbox_tickets = [['Monday, February 03, 2020 12:07:32 AM', 'HoWai Lam', '門市有得買？', 'Inbox'], ['Monday, February 03, 2020 12:07:21 AM', 'HoWai Lam', 'https://s3.amazonaws.com/spr-uploads-prod/815/FACEBOOK/82906/media/192A7B70A2A4B7997448C1AB747EE185', 'Inbox']]
+#
+# df_comment = pd.DataFrame(comment_tickets,columns=["Message_Time","Customer Name","Enquiry","URL"])
+#
+# df_inbox = pd.DataFrame(inbox_tickets,columns=["Message Time","Customer Name","Enquiry","URL"])
+#
+# df_comment["Message_Time"] = pd.to_datetime(df_comment.Message_Time)
+#
+# df_comment = df_comment.sort_values(by ='Message_Time',ascending=False)
+#
+# df_comment["Message_Time"] = df_comment["Message_Time"].dt.strftime("%A, %B %d, %Y %I:%M:%S %p")
+#
+# """print(x.strftime("%b %d %Y %H:%M:%S"))
+# Output:
+#
+# Sep 15 2018 00:00:00"""
+#
+# print(df_comment)
 
-msgtime_regex = re.compile(r'Posted on(.*)')
-ticket_time = msgtime_regex.search(posted).group(1)
+def delete_sheets():
 
-time = "Monday, February 03, 2020 12:07:32 AM"
+    export_sheet = ["Comment", "Inbox"]
+    wb_path = r'C:\Users\abc\PycharmProjects\somestuff\pandora_scraper\pandora_export.xlsx'
+    wb = openpyxl.load_workbook(wb_path)
+    sheets = wb.sheetnames
+    for i in export_sheet:
+        if i in sheets:
+            wb.remove(wb[i])
+    wb.save(wb_path)
 
-comment_tickets = [['Sunday, February 02, 2020 10:45:40 AM', 'Y L Chan', 'How much?', 'https://www.facebook.com/153753694819713/posts/1249108418617563/?comment_id=1250340885160983'],['Monday, February 03, 2020 11:29:26 AM', 'Chan Wing Yiu', r'https://scontent.xx.fbcdn.net/v/t1.0-9/s720x720/84003939_10157382231709440_3622552945624612864_n.jpg?_nc_cat=102&_nc_oc=AQlq6wf-34Mj-fjynxxUsqUjMdrT_ekpFIBT2Az0-3416A_X124IBKikqCPAeGvuhfY&_nc_ht=scontent.xx&oh=0fa985bb028b65bc4aaf3662ee0bddf4&oe=5EBB5493', r'https://www.facebook.com/153753694819713/posts/1249085878619817/?comment_id=1251058991755839'
-],['Monday, February 02, 2020 9:29:26 AM', 'Chan Wing Yiu', r'https://scontent.xx.fbcdn.net/v/t1.0-9/s720x720/84003939_10157382231709440_3622552945624612864_n.jpg?_nc_cat=102&_nc_oc=AQlq6wf-34Mj-fjynxxUsqUjMdrT_ekpFIBT2Az0-3416A_X124IBKikqCPAeGvuhfY&_nc_ht=scontent.xx&oh=0fa985bb028b65bc4aaf3662ee0bddf4&oe=5EBB5493', r'https://www.facebook.com/153753694819713/posts/1249085878619817/?comment_id=1251058991755839']]
+def df_to_list():
+    df = pd.DataFrame({'a': [1, 2, 3, 4],
+                       'b': [5, 6, 7, 8]})
 
-inbox_tickets = [['Monday, February 03, 2020 12:07:32 AM', 'HoWai Lam', '門市有得買？', 'Inbox'], ['Monday, February 03, 2020 12:07:21 AM', 'HoWai Lam', 'https://s3.amazonaws.com/spr-uploads-prod/815/FACEBOOK/82906/media/192A7B70A2A4B7997448C1AB747EE185', 'Inbox']]
+    print(df)
+    print(df.iterrows())
+    liss = []
+    for index,row in df.iterrows():
+        a = [row["a"],row["b"]]
+        liss.append(a)
+        print(a)
+    print(liss)
+    #print('list', [df[i].tolist() for i in df.iterrows()])
 
-df_comment = pd.DataFrame(comment_tickets,columns=["Message_Time","Customer Name","Enquiry","URL"])
+df_to_list()
 
-df_inbox = pd.DataFrame(inbox_tickets,columns=["Message Time","Customer Name","Enquiry","URL"])
 
-df_comment["Message_Time"] = pd.to_datetime(df_comment.Message_Time)
 
-df_comment = df_comment.sort_values(by ='Message_Time',ascending=False)
-
-df_comment["Message_Time"] = df_comment["Message_Time"].dt.strftime("%A, %B %d, %Y %I:%M:%S %p")
-
-"""print(x.strftime("%b %d %Y %H:%M:%S"))
-Output:
-
-Sep 15 2018 00:00:00"""
-
-print(df_comment)
